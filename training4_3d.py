675d10f6f62bd63e750d2705ef7a6af87807c26d
ardavurankaya <88663613+ardavurankaya@users.noreply.github.com>
2023-01-04 20:35:17 +0100

    Add files via upload

---
+++ training4_3d.py
@@ -0,0 +1,68 @@
+import numpy as np
+import torch
+from structure_tensor import eig_special_2d, structure_tensor_2d, eig_special_3d, structure_tensor_3d
+import skimage.io 
+import matplotlib.pyplot as plt
+import scipy as scp
+from network3_3d import network3_3d
+from torch.linalg import norm
+from util import utils
+from my_classes import Dataset 
+
+if torch.cuda.is_available():
+  device = 'cuda:0'
+else:
+  device = 'cpu'
+
+params = {'batch_size': 8,
+          'shuffle': True,
+          'num_workers': 2}
+
+training_ids = np.load('data/data_for_3_3d/training_ids.npy')
+validation_ids = np.load('data/data_for_3_3d/validation_ids.npy')
+test_ids = np.load('data/data_for_3_3d/test_ids.npy')
+
+training_set = Dataset(training_ids)
+training_generator = torch.utils.data.DataLoader(training_set, **params)
+
+validation_set = Dataset(validation_ids)
+validation_generator = torch.utils.data.DataLoader(validation_set, batch_size=len(validation_ids))
+
+num_conv = 3
+model = network3_3d(num_conv).to(device)
+
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
+
+total_epochs = 500
+val_loss_tracker = 0
+epoch = 0
+lowest_val_loss = np.inf
+
+print('Training starts!')
+
+while epoch < total_epochs and val_loss_tracker < 20:
+  epoch_loss = 0
+  for local_volumes, local_tensors, local_interpolations in training_generator:
+    local_volumes, local_tensors, local_interpolations = local_volumes.to(device), local_tensors.to(device), local_interpolations.to(device)
+    prediction = model.forward(local_volumes, local_interpolations)
+    loss = norm(prediction - local_tensors) / norm(local_tensors)
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    epoch_loss += loss.item()
+  with torch.no_grad():
+    for volume, tensor, interpolation in validation_generator:
+      volume, tensor, interpolation = volume.to(device), tensor.to(device), interpolation.to(device)
+      prediction = model.forward(volume, interpolation)
+      val_loss = norm(prediction - tensor) / norm(tensor)
+      if val_loss < lowest_val_loss:
+        print("New minimum of validation loss is reached!")
+        lowest_val_loss = val_loss
+        val_loss_tracker = 0
+        torch.save(model.state_dict(), 'Models3d/model4_3d_lr=1e-4_3conv.pth')
+  epoch += 1
+  val_loss_tracker = 0
+  print(f"Training loss at epoch {epoch} is {epoch_loss / 12} and validation_loss is {val_loss}")
+
+print('Training finished!')
+
---
+++ utils-2.py
@@ -0,0 +1,78 @@
+import numpy as np
+import torch
+from structure_tensor import eig_special_2d, structure_tensor_2d, eig_special_3d, structure_tensor_3d
+import skimage.io 
+import matplotlib.pyplot as plt
+import scipy as scp
+import cv2
+import torch
+import time
+from torch.nn.functional import interpolate
+
+def compute_rmse(ref, pred):
+  difference = (ref - pred) ** 2
+  error = np.sum(difference)
+  error = error / np.sum(ref ** 2)
+  return np.sqrt(error)
+
+
+def generate_volume(I, cube_size):
+  """
+  Takes original 3D volume(I) and divides data into smaller cubes denoted by cube_size
+  Also saves the created chunks as torch tensors
+  """
+  ind = 0 # data id
+  I -= I.mean()
+  print('Creating volumes...')
+  for i in range(int(I.shape[0] / cube_size)):
+    for j in range(int(I.shape[1] / cube_size)):
+      for k in range(int(I.shape[2] / cube_size)):
+        temp = I[i*cube_size:(i+1)*cube_size, j*cube_size:(j+1)*cube_size, k*cube_size:(k+1)*cube_size]
+        #temp = torch.from_numpy(temp)
+        np.save('data/data_for_3_3d/volume' + str(ind), temp)
+        ind += 1
+  print('Creating volumes finished!')
+
+def generate_structure_tensor(I, cube_size):
+  ind = 0
+  rho = 8
+  sigma = rho / 2
+  t0 = time.time()
+  print('Creating structure_ tensors...')
+#  S = structure_tensor_3d(I, rho, sigma)
+  for i in range(int(I.shape[0] / cube_size)):
+    for j in range(int(I.shape[1] / cube_size)):
+      for k in range(int(I.shape[2] / cube_size)):
+        temp = I[i*cube_size:(i+1)*cube_size, j*cube_size:(j+1)*cube_size, k*cube_size:(k+1)*cube_size]
+        #temp = torch.from_numpy(temp)
+        S = structure_tensor_3d(temp, rho, sigma)
+        np.save('data/data_for_3_3d/structure_tensor' + str(ind), S)
+        ind += 1  
+  print('Creating structure tensors finished!')
+
+def generate_interpolated_tensor(I, cube_size):
+  ind = 0
+  rho = 8
+  sigma = rho / 2
+  t0 = time.time()
+  print('Create interpolated tensors...')
+  t1 = time.time()
+  for i in range(int(I.shape[0] / cube_size)):
+    for j in range(int(I.shape[1] / cube_size)):
+      for k in range(int(I.shape[2] / cube_size)):
+        temp = I[i*cube_size:(i+1)*cube_size, j*cube_size:(j+1)*cube_size, k*cube_size:(k+1)*cube_size]
+        temp = torch.unsqueeze(torch.unsqueeze(torch.from_numpy(temp), 0), 0)
+        temp = interpolate(temp, scale_factor=(0.5, 0.5, 0.5))
+        temp = torch.squeeze(temp)
+        S = structure_tensor_3d(temp, rho, sigma)
+        S = torch.unsqueeze(torch.from_numpy(S), 0)
+        S = interpolate(S, scale_factor=(2,2,2), mode='trilinear')
+        S = torch.squeeze(S).numpy()
+        #temp = torch.from_numpy(temp)
+        np.save('data/data_for_3_3d/structure_tensor_interpolated' + str(ind), S)
+        ind += 1  
+  print(t1-t0)
+  print('Creating interpolated structure tensors finished!')
+
+
+
